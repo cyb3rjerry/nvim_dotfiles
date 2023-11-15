8111f5abc4d90a32535727d18387de29063b24c3
cyb3rjerry <cedric@cedricbrisson.ca>
2023-11-15 15:26:55 -0500

    feat: initial commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- Select
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- New tab
+keymap.set("n", "te", ":tabedit<Return>", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>")
+
+-- Split windows
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end)
